c40c7855f42b367e43fc2509f6b29570d457601b
SetrinaOlesia <setrina527@gmail.com>
2021-05-20 11:17:26 +0300

    first commit

---
+++ src/test/java/com/techprimers/testing/CucumberTest.java
@@ -0,0 +1,13 @@
+package com.techprimers.testing;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources",
+        glue = "com.techprimers.testing"
+)
+public class CucumberTest {
+}
